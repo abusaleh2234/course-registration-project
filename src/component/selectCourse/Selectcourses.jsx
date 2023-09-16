e0f16d547567892df15ba90dccc21dc5d4bffb31
abusaleh2234 <you@example.com>
2023-09-16 12:49:00 +0600

    course registration project complete

--- src/component/selectCourse/Selectcourses.jsx
+++ src/component/selectCourse/Selectcourses.jsx
@@ -1,6 +1,7 @@
 import  PropTypes  from "prop-types";
 const Selectcourses = ({selecteds,newcredit,remaining}) => {
     // console.log(selecteds)
+    let count = 1
     return (
         <div className="  w-full p-8 space-y-5">
             <div className="border-b-2 pb-6">
@@ -11,7 +12,7 @@ const Selectcourses = ({selecteds,newcredit,remaining}) => {
                 <h3 className="text-xl	font-bold pb-4 ">Course Name</h3>
                 <div className="pb-4 space-y-2">
                 {
-                    selecteds.map((select,idx) => <h4 key={idx}>{select.title}</h4>)
+                    selecteds.map((select,idx) => <h4 key={idx}>{count++}. {select.title}</h4>)
                 }
                 </div>
             </div>
